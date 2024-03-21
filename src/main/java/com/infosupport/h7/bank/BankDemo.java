75881d82a0990976b6e546d14a3c82aef7d13498
Bram Janssens <s.a.janssens@gmail.com>
2024-03-21 16:07:29 +0100

    Fix.

--- src/main/java/com/infosupport/h7/bank/BankDemo.java
+++ src/main/java/com/infosupport/h7/bank/BankDemo.java
@@ -3,9 +3,9 @@ package com.infosupport.h7.bank;
 public class BankDemo {
 
     public static void main(String[] args) {
-        Bank rabo = new Bank();
-        Account ac1 = new Account();
-        Account ac2 = new Account();
+        Bank rabo = Database.banks.get(0);
+        Account ac1 = Database.accounts.get(0);
+        Account ac2 = Database.accounts.get(1);
 
         // Fluent API:
         rabo.transaction()
