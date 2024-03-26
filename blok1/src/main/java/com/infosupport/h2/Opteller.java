7695c0db15a6be2c7bc905710e3bc32e177d7b43
Bram Janssens <s.a.janssens@gmail.com>
2024-03-26 10:12:34 +0100

    Fixed test.

--- blok1/src/main/java/com/infosupport/h2/Opteller.java
+++ blok1/src/main/java/com/infosupport/h2/Opteller.java
@@ -2,11 +2,7 @@ package com.infosupport.h2;
 
 public class Opteller {
 
-    static {
-        System.exit(0);
-    }
-
     public static int som(int a, int b) {
-        return a + b + 42;
+        return a + b;
     }
 }
