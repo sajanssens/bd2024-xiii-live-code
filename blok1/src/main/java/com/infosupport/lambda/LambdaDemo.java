73092ff96a3e430e6ce6bdabe7ed42e38b2b1ea5
Bram Janssens <s.a.janssens@gmail.com>
2024-04-02 14:08:55 +0200

    Lambda demo.

---
+++ blok1/src/main/java/com/infosupport/lambda/LambdaDemo.java
@@ -0,0 +1,68 @@
+package com.infosupport.lambda;
+
+import com.infosupport.h7.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+
+import static com.infosupport.streams.StreamDemo.people;
+
+public class LambdaDemo {
+
+    public static void main(String[] args) {
+        IntFunction<Integer> f = x -> x * x;
+        Consumer<Integer> c = (Integer i) -> System.out.println(i);
+
+        double result = f.apply(2);
+        long round = Math.round(1.2);
+
+        people.stream()
+                .map(p -> p.getAge())
+                .forEach(a -> System.out.println(a));
+
+        List<Person> resultToo =
+                selecteer(people,
+                        p -> {
+                            System.out.println(p);
+                            System.out.println("Hello");
+                            return p.getAge() >= 18;
+                        }
+                );
+
+        selecteer(people, p -> p.getName().startsWith("A"));
+        selecteer(people, p -> p.getName().startsWith("A") && p.getAge() >= 18 && p.getAge() <= 65);
+    }
+
+    public List<Person> selecteerOpLeeftijd(List<Person> people) {
+        List<Person> selectie = new ArrayList<>();
+        for (Person person : people) {
+            if (person.getAge() >= 18) {
+                selectie.add(person);
+            }
+        }
+        return selectie;
+    }
+
+    public List<Person> selecteerOpNaam(List<Person> people) {
+        List<Person> selectie = new ArrayList<>();
+        for (Person person : people) {
+            if (person.getName().startsWith("A")) {
+                selectie.add(person);
+            }
+        }
+        return selectie;
+    }
+
+    public static List<Person> selecteer(List<Person> people, Predicate<Person> conditie) {
+        List<Person> selectie = new ArrayList<>();
+        for (Person person : people) {
+            if (conditie.test(person)) {
+                selectie.add(person);
+            }
+        }
+        return selectie;
+    }
+}
