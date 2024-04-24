487543ea8d3b7d96dc470c1fec3e63a9fdb8612c
Bram Janssens <s.a.janssens@gmail.com>
2024-04-24 15:47:06 +0200

    Test Containers example.

---
+++ blok2/jpa/src/it/java/com/infosupport/EmployeeDaoTCIT.java
@@ -0,0 +1,43 @@
+package com.infosupport;
+
+import com.infosupport.domain.Employee;
+import com.infosupport.domain.EmployeeDao;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MySQLContainer;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EmployeeDaoTCIT {
+
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:latest");
+
+    private final EntityManager em = Persistence.createEntityManagerFactory("MySQL").createEntityManager();
+
+    private final EmployeeDao dao = new EmployeeDao(em);
+
+    @BeforeAll
+    static void beforeAll() {
+        mySQLContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mySQLContainer.stop();
+    }
+
+    @Test
+    void findByName_on_created_employee_returns_that_employee() {
+        dao.create(new Employee("Baby", LocalDate.now(), 20));
+        List<Employee> emps = dao.findBy("Baby");
+
+        // assertThat(emps.size()).isEqualTo(1);
+        assertThat(emps.getFirst().getName()).isEqualTo("Baby");
+    }
+}
