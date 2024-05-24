caf8948ea5b7ef448d81ccfd523c9e3add828ecd
Bram Janssens <s.a.janssens@gmail.com>
2024-05-24 12:03:54 +0200

    Added routing.

--- blok3/ng/baseball-quiz/src/app/app.routes.ts
+++ blok3/ng/baseball-quiz/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AdminComponent} from "./components/admin/admin.component";
+import {HomeComponent} from "./components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'admin', component: AdminComponent},
+];
