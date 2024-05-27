7026257e737f3669e1fa227e8129292ab1c23e14
Bram Janssens <s.a.janssens@gmail.com>
2024-05-27 12:00:16 +0200

    Added paginator.

--- blok3/ng/baseball-quiz/src/app/components/questions/questions.component.ts
+++ blok3/ng/baseball-quiz/src/app/components/questions/questions.component.ts
@@ -4,6 +4,7 @@ import {Question} from "../../domain/Question";
 import {QuestionService} from "../../services/question.service";
 import {Observable} from "rxjs";
 import {AsyncPipe, DatePipe, LowerCasePipe, UpperCasePipe} from "@angular/common";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
 
 @Component({
   selector: 'bq-questions',
@@ -13,13 +14,19 @@ import {AsyncPipe, DatePipe, LowerCasePipe, UpperCasePipe} from "@angular/common
     AsyncPipe,
     UpperCasePipe,
     LowerCasePipe,
-    DatePipe
+    DatePipe,
+    MatPaginator
   ],
   templateUrl: './questions.component.html',
   styleUrl: './questions.component.scss'
 })
 export class QuestionsComponent implements OnInit {
   questions$: Observable<Question[]> | undefined;
+  questions: Question[] = [];
+
+  totalItems = 100;
+  pageSize = 10;
+  currentPage = 0;
 
   constructor(private questionService: QuestionService) {
 
@@ -31,6 +38,16 @@ export class QuestionsComponent implements OnInit {
 
   getAllQuestions() {
     this.questions$ = this.questionService.findAll();
+    this.questions$.subscribe(r => {
+      this.totalItems = r.length
+      this.questions = r.slice(this.currentPage, this.currentPage + this.pageSize);
+    })
+  }
+
+  pageChanged(event: PageEvent) {
+    this.currentPage = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.getAllQuestions()
   }
 
 }
