65b05be8bdcd275a47395c9fbbf73a77efee5a03
Bram Janssens <s.a.janssens@gmail.com>
2024-05-27 13:37:30 +0200

    Server url centralized.

--- blok3/ng/baseball-quiz/src/app/services/question.service.ts
+++ blok3/ng/baseball-quiz/src/app/services/question.service.ts
@@ -2,32 +2,37 @@ import {Injectable} from '@angular/core';
 import {Question} from "../domain/Question";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {serverUrl} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
 
+  private url = serverUrl
+  private resource = '/questions';
+  private questionsPath = this.url + this.resource;
+
   constructor(private httpClient: HttpClient) {
   }
 
   findAll(): Observable<Question[]> { // R
-    return this.httpClient.get<Question[]>('http://localhost:3000/questions');
+    return this.httpClient.get<Question[]>(this.questionsPath);
   }
 
   find(id: number): Observable<Question> { // R
-    return this.httpClient.get<Question>(`http://localhost:3000/questions/${id}`);
+    return this.httpClient.get<Question>(`${this.questionsPath}/${id}`);
   }
 
   add(q: Question) { // C
-    return this.httpClient.post<Question>(`http://localhost:3000/questions`, q, {observe: 'response'} /* = to receive the full httpresponse including the token as http header, instead of only the body */);
+    return this.httpClient.post<Question>(this.questionsPath, q, {observe: 'response'} /* = to receive the full httpresponse including the token as http header, instead of only the body */);
   }
 
   remove(id: number) { // D
-    return this.httpClient.delete<Question>(`http://localhost:3000/questions/${id}`);
+    return this.httpClient.delete<Question>(`${this.questionsPath}/${id}`);
   }
 
   update(id: number, q: Question) { // U
-    return this.httpClient.put<Question>(`http://localhost:3000/questions/${id}`, q, {observe: 'response'});
+    return this.httpClient.put<Question>(`${this.questionsPath}/${id}`, q, {observe: 'response'});
   }
 }
